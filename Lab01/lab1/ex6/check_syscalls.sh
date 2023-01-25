1ba5d94ac0b756ca4be17d03fe9cc7a9f01951a2
David <david@dzhy.dev>
2023-01-25 20:07:08 +0800

    Completed ex6

--- Lab01/lab1/ex6/check_syscalls.sh
+++ Lab01/lab1/ex6/check_syscalls.sh
@@ -2,9 +2,9 @@
 
 ####################
 # Lab 1 Exercise 6
-# Name: 
-# Student No: 
-# Lab Group: 
+# Name: David Zhu, Tan Yuan Wei
+# Student No: A0253167A, A0235417H
+# Lab Group: B01, B14
 ####################
 
 echo "Printing system call report"
@@ -13,4 +13,4 @@ echo "Printing system call report"
 gcc -std=c99 pid_checker.c -o ex6
 
 # Use strace to get report
-
+strace -c ./ex6
